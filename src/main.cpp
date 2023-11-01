400744f584b126c29e8faaf3ed08f32450a87f00
CiproGit <76820530+CiproGit@users.noreply.github.com>
2023-11-01 16:16:29 +0100

    Modularize capitalization

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,11 @@ void print_about() {
 	cout << "******************************\n" << endl;
 }
 
+void capitalize(QString &class_name) {
+	cout << "Warning: class names should start with capital letters. I will handle that" << endl;
+	class_name.replace(0, 1, class_name.at(0).toUpper()); // Capitalize first character
+}
+
 QString sanitize(string input) {
 	if (input.back() == '\"') { // The last character of the input string is "
 		input = input.substr(0, input.size() - 1); // Remove the last character
@@ -29,10 +34,7 @@ int main(int argc, char *argv[]) {
 
 	if (argc == 2) { // 1 argument
 		QString class_name = argv[1];
-		if (class_name.at(0).isLower()) {
-			cout << "Warning: class names should start with capital letters. I will handle that" << endl;
-			class_name.replace(0, 1, class_name.at(0).toUpper()); // Capitalize first character
-		}
+		if (class_name.at(0).isLower()) capitalize(class_name);
 
 		class_creator::Class_creator class_creator(class_name);
 		class_creator.print_data();
@@ -41,10 +43,7 @@ int main(int argc, char *argv[]) {
 	}
 	else if (argc == 3) { // 2 arguments
 		QString class_name = argv[1];
-		if (class_name.at(0).isLower()) {
-			cout << "Warning: class names should start with capital letters. I will handle that" << endl;
-			class_name.replace(0, 1, class_name.at(0).toUpper()); // Capitalize first character
-		}
+		if (class_name.at(0).isLower()) capitalize(class_name);
 
 		QString project_directory = sanitize(argv[2]);
 
